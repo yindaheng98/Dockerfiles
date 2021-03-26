1524660a7ab546108fff55b36915bb686250386f
yindaheng98 <yindaheng98@163.com>
2021-03-26 09:39:16 +0800

    更新frp

---
+++ frp/frps/write-ini.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cat > /frps.ini << EOF
+[common]
+bind_port = 7000
+
+# set dashboard_addr and dashboard_port to view dashboard of frps
+# dashboard_addr's default value is same with bind_addr
+# dashboard is available only if dashboard_port is set
+dashboard_addr = 0.0.0.0
+dashboard_port = 7500
+
+# dashboard user and passwd for basic auth protect, if not set, both default value is admin
+EOF
+
+echo "dashboard_user = ${dashboard_user}" >> /frps.ini
+echo "dashboard_pwd = ${dashboard_pwd}" >> /frps.ini
